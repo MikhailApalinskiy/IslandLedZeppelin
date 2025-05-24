6133969ed4f8aacec2a2f0aead72a6f6e882b969
Mikhail Apalinski <apalinskiy.mihkail@gmail.com>
2025-05-24 05:01:05 +0400

    CHANGE Map & Cell

--- src/main/java/com/javarush/island/apalinskiy/map/Cell.java
+++ src/main/java/com/javarush/island/apalinskiy/map/Cell.java
@@ -1,16 +1,58 @@
 package com.javarush.island.apalinskiy.map;
 
 import com.javarush.island.apalinskiy.creatures.Creature;
+import com.javarush.island.apalinskiy.creatures.animals.Animal;
+import com.javarush.island.apalinskiy.creatures.plants.AbstractPlant;
+import lombok.Getter;
 
 import java.util.HashSet;
+import java.util.Set;
 
+@Getter
 public class Cell {
-    HashSet<Creature> creatures = new HashSet<>();
+    private final int x;
+    private final int y;
 
-    public int findAMeal(){
-        for (Creature creature : creatures) {
-            
+    private final HashSet<Animal> animals = new HashSet<>();
+    private final HashSet<AbstractPlant> plants = new HashSet<>();
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+        animal.setCurrentCell(this);
+    }
+
+    public void removeAnimal(Animal animal) {
+        animals.remove(animal);
+        animal.setCurrentCell(null);
+    }
+
+    public void addPlant(AbstractPlant plant){
+        plants.add(plant);
+        plant.setCurrentCell(this);
+    }
+
+    public void removePlant(AbstractPlant plant){
+        plants.remove(plant);
+        plant.setCurrentCell(null);
+    }
+
+    public Set<Creature> getCreatures() {
+        Set<Creature> all = new HashSet<>();
+        all.addAll(animals);
+        all.addAll(plants);
+        return all;
+    }
+
+    public void removeCreature(Creature creature) {
+        if (creature instanceof Animal animal) {
+            removeAnimal(animal);
+        } else if (creature instanceof AbstractPlant plant) {
+            removePlant(plant);
         }
-        return 0;
     }
 }
--- src/main/java/com/javarush/island/apalinskiy/map/Map.java
+++ src/main/java/com/javarush/island/apalinskiy/map/Map.java
@@ -1,8 +1,18 @@
 package com.javarush.island.apalinskiy.map;
 
-public class Map {
-    private final int Length = 100;
-    private final int width = 20;
-    private final Cell[][] map = new Cell[100][20];
+import lombok.Getter;
 
+@Getter
+public class Map {
+    private final int length = 100;
+    private final int width = 20;
+    private final Cell[][] map = new Cell[length][width];
+
+    public void initializeMap(){
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                map[i][j] = new Cell(i, j);
+            }
+        }
+    }
 }
